e97ff69a106455bb4d7caa44fd81a3a677cb80e0
minh1622 <huytranngoc098@gmail.com>
2021-06-13 16:51:55 +0700

    new

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.get('/', (req,res)=>{
     res.render('homepage')
 })
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, ()=>{
+var server_port = process.env.PORT || 3000;
+app.listen(server_port, ()=>{
     console.log(`server listen on ${server_port}`)
 })
