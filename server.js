3807caebac787ab3e03d802dc97d921a7ff35de0
minh1622 <huytranngoc098@gmail.com>
2021-06-13 16:45:29 +0700

    a

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-port = process.env.port || 3000
 const myApp = require('./src/routes/App.router')
 
 app.use('/js', express.static(path.join(__dirname, '/public/js')));
@@ -12,6 +11,8 @@ app.get('/', (req,res)=>{
     res.render('homepage')
 })
 
-app.listen(port, ()=>{
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, ()=>{
     console.log(`server listen on ${port}`)
 })
